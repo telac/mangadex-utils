b890170c49406d9a4419b87fd6274bc83db054fe
Christian Westerlund <ca.westerlund@gmail.com>
2021-12-04 16:17:12 +0200

    update reader to reflect api changes

--- mangadex_downloader.py
+++ mangadex_downloader.py
@@ -12,6 +12,7 @@ from volumes_to_cbz import folder_to_cbz
 
 BASE_URI = "https://api.mangadex.org"
 QUALITY_MODE = "data"
+BASE_DELAY = 0.25
 
 def get_chapters(manga_id, limit=500):
     res = requests.get(f"https://api.mangadex.org/manga/{manga_id}/feed?limit={limit}&translatedLanguage[]=en").json()
@@ -22,7 +23,7 @@ def download_single_image(source_filename, at_home_url, chapter_hash, dest_filen
         with open(f"{dest_filename}", "wb") as dest_file:
             res = requests.get(f"{at_home_url}/{QUALITY_MODE}/{chapter_hash}/{source_filename}")
             dest_file.write(res.content)
-            sleep(0.5)
+            sleep(BASE_DELAY)
             return True
     except Exception:
         sleep(10)
@@ -45,7 +46,7 @@ def download_single_chapter(chapter_id, destination_path):
         if f"{destination_path}_{index}.png" in glob.glob(destination_path + '*.png'):
             print("file already in place, skipping")
         else:
-            download_single_image(filename, at_home_url, chapter_hash, f"{destination_path}_{index}.png")
+            download_single_image(filename, at_home_url, chapter_hash, f"{destination_path}_{str(index).zfill(4)}.png")
             
 
     
@@ -54,6 +55,7 @@ def download_chapter(id):
 
 def get_title(manga_id):
     res = requests.get(f"{BASE_URI}/manga/{manga_id}")
+    #print(res.json())
     return res.json()['data']['attributes']['title']['en']
 
 def clean_string(string):
@@ -65,14 +67,41 @@ def clean_string(string):
 def get_volumes_in_folder(folder):
     pass
 
+def get_cover_art(manga_id, root_dir):
+    cover_ids = requests.get(f"https://api.mangadex.org/cover?manga[]={manga_id}").json()
+    print("downloading cover art")
+    logging.debug(cover_ids)
+    print(cover_ids)
+    for cover in cover_ids['data']:
+        attributes = cover['attributes']
+        volume = attributes['volume']
+        filename = attributes['fileName']
+        filetype = filename.split(".")[1]
+        cover_url = f"https://uploads.mangadex.org/covers/{manga_id}/{filename}"
+        volume = volume or "oneshot"
+        folders = glob.glob(root_dir + '/*/')
+        dest = f"{root_dir}/volume_{volume}/"
+        if len(folders) == 1:
+            dest = f"{folders[0]}"
+        Path(dest).mkdir(parents=True, exist_ok=True)
+        print(dest)
+        with open(f"{dest}/00000{volume}_coverart.{filetype}", "wb") as dest_file:
+            print(f"{dest}/00000{volume}_coverart.{filetype}")
+            res = requests.get(f"{cover_url}")
+            dest_file.write(res.content)
+            sleep(1)
+
 def download_all_chapters(manga_id, fpath, limit_volumes = [], start_chapter=None, end_chapter=None):
     manga_title = clean_string(get_title(manga_id))
-    chapters_response = get_chapters(manga_id)["results"]
+    chapters_response = get_chapters(manga_id)["data"]
+    num_chapters = len(chapters_response)
+    num_processed = 0
     logging.debug(chapters_response)
     chapters = {}
     for chapter in chapters_response:
         logging.debug(chapter)
-        chapter_data = chapter['data']
+        #print(chapter)
+        chapter_data = chapter # ['attributes']['data']
         chapter_id = chapter_data['id']
         volume = chapter_data["attributes"]["volume"]
         chapter = chapter_data["attributes"]["chapter"]
@@ -95,13 +124,21 @@ def download_all_chapters(manga_id, fpath, limit_volumes = [], start_chapter=Non
         volume = volume or "oneshot"
         dest = f"{fpath}/{manga_title}/volume_{volume}"
         Path(dest).mkdir(parents=True, exist_ok=True)
-        destination = f"{dest}/{chapter}"
-        if not glob.glob(destination + '*.png'):
-            with open(f"{dest}/chapter_{chapter}_metadata.json", "w") as metadata:
+        destination = f"{dest}/{str(chapter).zfill(4)}"
+        metadata_basename = os.path.basename(f"{dest}/chapter_{chapter}_metadata.json")
+        metadata_filename = f"{dest}/chapter_{chapter}_metadata.json"
+        chapters_downloaded = [os.path.basename(x) for x in glob.glob(dest + '/*.json')]
+        skipped = True
+        if metadata_basename not in chapters_downloaded:
+            skipped = False
+            with open(metadata_filename, "w") as metadata:
                 metadata.write(json.dumps(chapters[chapter_id]))
             download_single_chapter(chapter_id, destination)
-        print(f"downloaded chapter Vol. {volume}, chapter: {chapter}")
-    folder_to_cbz(fpath)
+        num_processed += 1
+        progress = (num_processed / num_chapters) * 100
+        print(f"PROGRESS {num_processed}/{num_chapters} [{progress} %]: downloaded chapter Vol. {volume}, chapter: {chapter} Skipped : {skipped}")
+    get_cover_art(manga_id, f"{fpath}/{manga_title}")
+    folder_to_cbz(f"{fpath}/{manga_title}")
 
 
 
--- volumes_to_cbz.py
+++ volumes_to_cbz.py
@@ -3,27 +3,106 @@ from glob import glob
 import os
 from pathlib import Path
 import argparse
+import logging
+
+def get_files(path):
+    png_files = glob(path + '/' + '*.png')
+    jpg_files = glob(path + '/' + '*.jpg')
+    files = png_files + jpg_files
+    return files
+
+def get_partial_files(path, glob_expr):
+    files = get_files(path)
+    partial_files = []
+    glob_expr = set(glob_expr)
+    for file in files:
+        basename = os.path.basename(file)
+        if basename in glob_expr:
+            partial_files.append(file)
+    return partial_files
+
+def to_chunks(l, n):
+    return [l[i:i + n] for i in range(0, len(l), n)]
 
 
 def to_cbz(filename, path):
-    files = glob(path + '/' + '*.png')
+    files = get_files(path)
     with ZipFile(f"{path}/{filename}", 'w') as zip_object:
         for f in files:
-            print(f"adding file {f} to zip")
+            logging.debug(f"adding file {f} to zip")
             zip_object.write(f, os.path.basename(f))
+        print(f"created CBZ archive at: {path}/{filename}")
 
+def files_to_cbz(filename, glob_expr, path):
+    files = get_partial_files(path, glob_expr)
+    with ZipFile(f"{path}/{filename}", 'w') as zip_object:
+        for f in files:
+            logging.debug(f"adding file {f} to zip")
+            zip_object.write(f, os.path.basename(f))
+        print(f"created CBZ archive at: {path}/{filename}")
+
+def get_chapters(files):
+    chapters = set()
+    for file in files:
+        try:
+            chapter = file.split("_")[0]
+            float(chapter)
+            chapters.add(chapter)
+        except ValueError:
+            continue
+    return sorted(chapters, key= lambda x: float(x))
+
+def merge_lists(d):
+    l = []
+    for key in d:
+        l = l + d[key]
+    return l
+    
 def folder_to_cbz(root):
     dirs = os.walk(root)
     for root, dirs, files in dirs:
         if files:
-            series_name = os.path.basename(Path(root).parent)
-            volume_name = os.path.basename(root)
-            fname = f"{series_name}_{volume_name}.cbz"
-            to_cbz(fname, root)
+            chapters = get_chapters(files)
+            if len(chapters) > 10:
+                chunks = to_chunks(chapters, 10)
+                for chunk in chunks:
+                    chunk_file_dict = {volume:[] for volume in chunk}
+                    for _file in files:
+                        volume = _file.split("_")[0]
+                        if volume in chunk_file_dict:
+                            chunk_file_dict[volume].append(_file)
+                    series_name = os.path.basename(Path(root).parent)
+                    volume_name = os.path.basename(root)
+                    chapter_min = float(min(chunk))
+                    chapter_max = float(max(chunk))
+                    fname = f"{series_name}_{volume_name}_c{chapter_min}_{chapter_max}.cbz"
+                    merged_files = merge_lists(chunk_file_dict)
+                    files_to_cbz(fname, merged_files, root)
+            else:
+                series_name = os.path.basename(Path(root).parent)
+                volume_name = os.path.basename(root)
+                fname = f"{series_name}_{volume_name}.cbz"
+                
+                to_cbz(fname, root)
+
+def batch_rename(path):
+    dirs = os.walk(path)
+    for root, dirs, files in dirs:
+        if files:
+            files_to_be_renamed = get_files(root)
+            for file in files_to_be_renamed:
+                print(file)
+                print(os.path.basename(file))
+                volume = os.path.basename(file).split("_")[0]
+                chapter = os.path.basename(file).split("_")[1]
+                chapter, filetype = chapter.split(".")
+                new_basename = f'{volume.zfill(5)}_{chapter.zfill(5)}.{filetype}'
+                os.rename(file, f"{root}/{new_basename}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create .cbz files to be used by comic readers")
     parser.add_argument('--path', help="root folder of the volumes", required=True)
     args = parser.parse_args()
+    #batch_rename(args.path)
     folder_to_cbz(args.path)
     
